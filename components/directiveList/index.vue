25661315fb2c096bfe89cc9f845ad7aa286a4afd
ql17774641928 <2722937531@qq.com>
2023-03-25 21:38:03 +0800

    完成directiveList组件

---
+++ components/directiveList/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <view class="device-list">
+    <u-cell-group :border="false" class="cell-group">
+      <DirectiveItem v-for="directive in directiveList" :key="directive.id" :directive="directive" :deviceNumber="deviceNumber" @reset="reset"/>
+    </u-cell-group>
+  </view>
+</template>
+
+<script>
+import DirectiveItem from '@/components/directiveItem/index.vue'
+export default {
+  components: {
+    DirectiveItem
+  },
+  props: {
+    directiveList: {
+      type: Array,
+      default: () => []
+    },
+    deviceNumber: {
+      type: String,
+    }
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    reset() {
+      this.$emit('reset')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.device-list-container {
+}
+</style>
