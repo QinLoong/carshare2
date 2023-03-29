cb7c5ae7e0b432e6c566c79b21ead6ac89fd5787
ql17774641928 <2722937531@qq.com>
2023-03-29 16:44:54 +0800

    编写登陆组件

---
+++ pages/login/index.vue
@@ -0,0 +1,72 @@
+<template>
+    <view class="login-wrap">
+      <view class="top-status-bar" />
+      <view class="login-logo">
+        <image mode="" src="~@/static/images/logo.svg" />
+      </view>
+      <view class="login-title">账号密码登录</view>
+      <view class="login-form">
+        <u--form ref="loginForm" error-type="toast" label-position="left" :model="loginInfo" :rules="rules">
+          <view :class="{ 'form-item': true }">
+            <u-form-item prop="userPhone">
+              <u-input v-model="loginInfo.userPhone" placeholder="账号" @blur="blurHandle('userPhone')" @focus="focusHandle('userPhone')">
+                <template slot="prefix">
+                  <text class="iconfont icon-account prefix-icon" />
+                </template>
+              </u-input>
+            </u-form-item>
+          </view>
+  
+          <view :class="{ 'form-item': true }">
+            <u-form-item prop="password">
+              <u-input v-model="loginInfo.password" placeholder="密码" :type="pwdShow ? 'text' : 'password'" @blur="blurHandle('password')" @focus="focusHandle('password')">
+                <template slot="prefix">
+                  <text class="iconfont icon-password prefix-icon" />
+                </template>
+                <template slot="suffix">
+                  <text class="iconfont prefix-icon" :class="pwdShow ? 'icon-pwdyc' : 'icon-pwdxs'" @click="pwdShow = !pwdShow" />
+                </template>
+              </u-input>
+            </u-form-item>
+          </view>
+  
+          <view class="form-item">
+            <view class="toggle-login flex-c" @click="isLogin = !isLogin"> {{ !isLogin ?  '登录' : '注册'}}</view>
+            <u-form-item>
+              <u-button shape="circle" :text="isLogin ? '登录' : '注册'" type="primary" @click="submit" />
+            </u-form-item>
+          </view>
+        </u--form>
+      </view>
+    </view>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        loginInfo: {
+          userPhone: '',
+          password: ''
+        },
+        rules: {
+          userPhone: [
+            {
+              required: true,
+              message: '请输入账号'
+            }
+          ],
+          password: [
+            {
+              required: true,
+              message: '请输入密码'
+            }
+          ]
+        },
+        codeFocus: false,
+        passwordFocus: false,
+        pwdShow: false,
+        isLogin: true
+      }
+    }
+}
