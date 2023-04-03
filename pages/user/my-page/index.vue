b331b78a7f623aed76cde72838d5988e78961227
ql17774641928 <2722937531@qq.com>
2023-04-03 17:23:47 +0800

    完成用户主页的汽车列表展示

--- pages/user/my-page/index.vue
+++ pages/user/my-page/index.vue
@@ -11,3 +11,47 @@
       <u-picker :show="location.show" :columns="[location.locations]" itemHeight="60" closeOnClickOverlay @close="location.show = false" @cancel="location.show = false" @confirm="findBikeList"></u-picker>
     </view>
   </template>
+  
+  <script name="OrderPage">
+  import { findBikeList } from '@/common/api/device.js'
+  import { locations } from '@/common/utils/dict'
+  import cyc from '../cycling-page/cyc.vue'
+  export default {
+    components: { cyc },
+    data() {
+      return {
+        userId: this.$store.state.userInfo.id,
+        cycList: [],
+        location: {
+          locations,
+          show: false
+        }
+      }
+    },
+    mounted() {
+      this.findBikeList()
+    },
+    methods: {
+      async findBikeList(v) {
+        try {
+          const location = v?.value?.length ? v.value[0] : null
+          const res = await findBikeList({ page: 1, size: 1000, ...(!location || location === '' || location === '全部' ? {} : { location }) })
+          if (res.code === 200) {
+            this.cycList = res.data.records
+          } else {
+            this.$toast.error('请求失败!')
+          }
+        } catch (err) {
+          this.$toast.error(err)
+        } finally {
+          this.location.show = false
+        }
+      },
+      navTo(url, id) {
+        uni.redirectTo({
+          url: url + '?page=homePage&id=' + id
+        })
+      }
+    }
+  }
+  </script>
