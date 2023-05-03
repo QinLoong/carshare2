c7cc16f0d312ca5a9ee2521f7796a2ced590a53e
ql17774641928 <2722937531@qq.com>
2023-05-03 14:26:56 +0800

    编写底部导航栏及路由

---
+++ pages/admin/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <view class="content">
+      <view class="top-status-bar" />
+      <view class="main">
+        <StatisticsPage v-if="this.routeActive === 0" />
+        <DevicePage v-if="this.routeActive === 1" />
+        <DataPage v-if="this.routeActive === 2" />
+        <FaultPage v-if="this.routeActive === 3" />
+        <MyPage v-if="this.routeActive === 4" />
+      </view>
+  
+
+  </template>
+  
+  <script>
+  import StatisticsPage from './statistics-page/index'
+  import DevicePage from './device-page/index'
+  import MyPage from './my-page/index'
+  import FaultPage from './fault-page/index'
+  import DataPage from './data-page/index'
+  export default {
+  components: {
+    StatisticsPage,
+    DevicePage,
+    MyPage,
+    FaultPage,
+    DataPage
+  }
+}
+</script>
