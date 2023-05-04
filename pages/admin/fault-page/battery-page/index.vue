3830b408264eda8505a72a0ed182107b4557fabf
ql17774641928 <2722937531@qq.com>
2023-05-04 16:53:12 +0800

    搭建基本界面

---
+++ pages/admin/fault-page/battery-page/index.vue
@@ -0,0 +1,14 @@
+<template>
+	<view class="user-record-container">
+		<ScrollTolower ref="scrollTolowerRef" @request="findUserList" :loadmoreMarginTop="20"
+			:loadmoreMarginBottom="20">
+			<template slot="main">
+				<view class="user-image">
+					<image src="@/static/images/user-page/user.svg"></image>
+				</view>
+			</template>
+		</ScrollTolower>
+	</view>
+</template>
+<script name="userRecord">
+</script>
