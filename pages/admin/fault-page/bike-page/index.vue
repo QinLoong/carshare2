c3540c304920dce81b26292649932cf88a322247
ql17774641928 <2722937531@qq.com>
2023-05-10 14:33:02 +0800

    搭建电车界面

---
+++ pages/admin/fault-page/bike-page/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<view class="user-record-container">
+		<ScrollTolower ref="scrollTolowerRef" @request="findUserList" :loadmoreMarginTop="20"
+			:loadmoreMarginBottom="20">
+			<template slot="main">
+				<view class="user-image">
+					<image src="@/static/images/user-page/user.svg"></image>
+				</view>
+				
+			</template>
+		</ScrollTolower>
+	</view>
+</template>
