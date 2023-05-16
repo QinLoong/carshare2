72b69c829f121e70ee98ca130d376ccbd4ebe0af
ql17774641928 <2722937531@qq.com>
2023-05-16 16:38:58 +0800

    添加时间戳转换

--- pages/admin/fault-page/bike-page/index.vue
+++ pages/admin/fault-page/bike-page/index.vue
@@ -58,6 +58,19 @@
 			}
 		},
 		methods: {
+            formatTimestampToCustomString(timestamp) {
+			  const date = new Date(timestamp);
+			  const year = date.getFullYear();
+			  const month = date.getMonth() + 1; // 月份从0开始，需要加1
+			  const day = date.getDate();
+			  const hours = date.getHours();
+			  const minutes = String(date.getMinutes()).padStart(2, '0'); // 使用padStart确保两位数格式
+			  const seconds = String(date.getSeconds()).padStart(2, '0'); // 使用padStart确保两位数格式
+			
+			  const formattedDate = `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
+			
+			  return formattedDate;
+			},
             confirm2(e) {
 				
 				this.inputTime = this.formatTimestampToCustomString(e.value),
