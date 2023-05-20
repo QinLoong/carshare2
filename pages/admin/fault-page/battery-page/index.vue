1222a4078d846ea6f1bebe68bcc5918ddf6d03bc
ql17774641928 <2722937531@qq.com>
2023-05-20 20:17:39 +0800

    为battery添加样式

--- pages/admin/fault-page/battery-page/index.vue
+++ pages/admin/fault-page/battery-page/index.vue
@@ -178,3 +178,36 @@
 			},
 		},
 </script>
+<style lang="scss" scoped>
+	.user-record-container {
+		height: calc(100vh - 90rpx);
+
+		.user-image {
+			width: 100%;
+			height: 300rpx;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			background: linear-gradient($color1, $color2);
+
+			image {
+				padding: 30rpx;
+				width: 210rpx;
+				height: 70%;
+				margin: auto;
+				background: rgba(255, 255, 255, 0.2);
+				border-radius: 50%;
+			}
+		}
+
+		.user-value {
+			color: $uni-color-primary;
+		}
+
+		::v-deep {
+			.u-cell-group__title {
+				background: $app-background-grey;
+			}
+		}
+	}
+</style>
