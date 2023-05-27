471319c4fdd0492b44b44c8f27900f70502ebbb4
ql17774641928 <2722937531@qq.com>
2023-05-27 21:37:11 +0800

    基本骨架

---
+++ pages/admin/fault-page/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <view class="fault-record-container">
+      <ScrollTolower ref="scrollTolowerRef" @request="findFaultList" :loadmoreMarginTop="20" :loadmoreMarginBottom="20">
+       
+      </ScrollTolower>
+    </view>
+  </template>
